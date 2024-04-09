0b9182038af3982804ca927e3461d4e012eea5d5
yelchuridinesh <yelchuri.d@northeastern.edu>
2024-04-09 14:38:55 -0400

    1st stage

--- app/main.go
+++ app/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage!
-	// "os"
-	// "os/exec"
+	Uncomment this block to pass the first stage!
+	"os"
+	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
